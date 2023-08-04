18943a5b534b5de2b183066a70f50e4d2b78e994
rima1881 <amd.gharibipour@outlook.com>
2023-08-04 13:41:19 -0400

    student_staff added

---
+++ models/Student_Staff.cpp
@@ -0,0 +1,21 @@
+#include "./Student_Staff.hpp"
+#include "./Student.hpp"
+#include "./Staff.hpp"
+
+Student_Staff::Student_Staff(string student_db_addr,string staff_db_addr){
+
+
+
+}
+
+void Student_Staff::Highest_GPA(Student* students){
+
+}
+
+void Student_Staff::Same_Hire_Year(Staff* staff){
+
+}
+
+Student* Student_Staff::N_of_UnderGrad(Student* Students){
+
+}
--- models/Student_Staff.hpp
+++ models/Student_Staff.hpp
@@ -1,8 +1,25 @@
+#ifndef Student_Staff
+
+#include "./Student.hpp"
+#include "./Staff.hpp"
+
 class Student_Staff
 {
 private:
-    /* data */
+
+    Student* std_list;
+    Staff* staff_list;
+
 public:
-    Student_Staff(/* args */);
-    ~Student_Staff();
+
+    //constructor
+    Student_Staff(string student_db_addr,string staff_db_addr);
+
+    //static mothods
+    static void Highest_GPA(Student* students);
+    static Student* N_of_UnderGrad(Student* students);
+    static void Same_Hire_Year(Staff* staff);
+
 };
+
+#endif
